e05b134ca6e54f70248acd93c8604c107c275fce
Zayed <zayed.cs3@gmail.com>
2021-06-15 18:36:57 +0600

    Operator File Added.

---
+++ operator.js
@@ -0,0 +1,43 @@
+// Arithmetic Operators
+// +, -, *, /, %
+
+console.log(5+5)
+console.log(5-5)
+console.log(5*5)
+console.log(5/5)
+console.log(10%3) // modulus operator ; returns remainder
+
+// Increment, Decrement
+// ++, --
+
+let x = 1000
+
+console.log(x++) // Post Increment; prints 1000 & increment 1
+console.log(x--) //Post Decrement; prints 1001 and decrement 1
+
+
+x += 40 // x = x + 40
+
+console.log(40)
+
+
+// Comparison Operators
+// <, >, ==, <=, >=, !=, ===
+// Returns either True or False
+
+
+let p = 1000
+let q = '1000'
+
+console.log(p == q) // Return True, type doesn't matter
+console.log(p === q) // Returns False, type matters.
+console.log(p !== q)  // Returns True
+
+// Logical Operators
+// &&, ||, !(Converts positive to Negative and vice-versa)
+
+// Bitwise Operators
+// &, |, ~, ^, <<, >>
+
+// typeof operator
+// used to identify types of a variable
