cb117b77a407c3f031e86912fe758c6c411a31b5
Zayed <zayed.cs3@gmail.com>
2021-06-15 20:02:51 +0600

    Else If logic added.

---
+++ else_if.js
@@ -0,0 +1,14 @@
+var firstNumber = 100
+var secondNumber = 50
+
+if (firstNumber > lastNumber)
+{
+    console.log("First Number is Greater than the Second")
+}
+else if (firstNumber === lastNumber)
+{
+    console.log("Numbers are equal")
+}
+else{
+    console.log("First Number is smaller than the second. ")
+}
