35ee3d706e297da20109bc7f8248f6fa48058ccf
Zayed <zayed.cs3@gmail.com>
2021-06-15 19:59:57 +0600

    Else logic added.

---
+++ else.js
@@ -0,0 +1,16 @@
+var a = 100
+var b = 5
+
+if (a<b){
+    console.log("A is less than B")
+}
+else{
+    console.log("A is greater than B")
+}
+
+if(b%2===0){
+    console.log("B is Even")
+}
+else{
+    console.log("B is ODD")
+}
