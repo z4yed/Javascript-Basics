1d46822bd82c69c5857ca71ced3d1182ee6dcd5f
Zayed <zayed.cs3@gmail.com>
2021-06-15 20:21:49 +0600

    Ternary operator Added.

---
+++ ternary_operator.js
@@ -0,0 +1,14 @@
+var n = 101
+// var str = ''
+
+// if (n%2===0){
+//     str = "EVEN"
+// }
+// else{
+//     str = "ODD"
+// }
+// console.log(str)
+
+var str = n%2===0 ? "EVEN" : "ODD";
+// condition ? true: false;
+console.log(str)
