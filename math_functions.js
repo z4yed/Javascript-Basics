fbf2e8e27d5690e20e3b08b0a56d0e612978c946
Zayed <zayed.cs3@gmail.com>
2021-06-15 19:44:04 +0600

    Build In Math Functions Added.

---
+++ math_functions.js
@@ -0,0 +1,16 @@
+console.log(Math.E)
+console.log(Math.PI)
+console.log(Math.ceil(5.253))  // 6
+console.log(Math.floor(20.3523)) // 20
+console.log(Math.round(5.253)) // 5
+console.log(Math.round(5.788)) // 6
+
+
+let x = -10.99
+console.log(Math.abs(x)) // makes positive
+
+console.log(Math.max(1, 2, 3, 1000)) // Any number of arguments
+console.log(Math.min(1, 2, 3))
+console.log(Math.pow(10, 2)) // 10 to the power 2
+console.log(Math.sqrt(9))  // square root
+console.log(Math.random()) // random between 0 to 1
