44b9ead1f6e8c29d56da83bde85f8059bccf9d7a
Zayed <zayed.cs3@gmail.com>
2021-06-15 19:57:25 +0600

    If Logic Added.

---
+++ if.js
@@ -0,0 +1,22 @@
+var a = 1000
+var b = 20
+
+if (a > b){
+    console.log("A is greater than B")
+}
+
+if (a < b){
+    console.log("A is less than B")
+}
+
+if (a == b){
+    console.log("A is equal to B")
+}
+
+if ( a%2 === 0){
+    console.log("A is Even")
+}
+
+if (a%2 === 0){
+    console.log("A is Odd.")
+}
